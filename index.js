1b4cec256f2b98917a3b78f43a8e76800e72ba6a
Tran Khoa <khoalt.pht@gmail.com>
2020-08-31 23:18:11 +0700

    fix done

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.get('/', function(req,res) {
 
 app.get('/user', function(req,res) {
     res.send('User list');  
-  });
-  app.get('/user', function(req,res) {
-    res.send('User list');  
-  });
+});
 
 app.listen(port, function() {
     console.log('server listening on port ' + port)
