d2de87e572e1652807db256960530e41324e3c3f
Tran Khoa <khoalt.pht@gmail.com>
2020-08-31 23:11:52 +0700

    add get

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.get('/', function(req,res) {
   res.send('<h1>Hello world</h1>');  
 });
 
+app.get('/user', function(req,res) {
+    res.send('User list');  
+  });
 
+  app.get('/user', function(req,res) {
+    res.send('User lists');  
+  });
 
 app.listen(port, function() {
     console.log('server listening on port ' + port)
