80610241f5d29ca26a7cfb83df7273e344896d57
Tran Khoa <khoalt.pht@gmail.com>
2020-08-31 23:00:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', function(req,res) {
+  res.send('<h1>Hello world</h1>');  
+});
+
+app.get('/user', function(req,res) {
+    res.send('Uesr list');  
+  });
+
+app.listen(port, function() {
+    console.log('server listening on port ' + port)
+});
