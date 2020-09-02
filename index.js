291d22f1f902be35d38ae4eaa4e5e21641b81eeb
Tran Khoa <khoalt.pht@gmail.com>
2020-09-02 18:41:09 +0700

    learn-query

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = 3000;
 
+var users = [
+  {id:1, name:'Khoa'},
+  {id:2, name:'Khoa2'},
+];
+
 app.set('view engine','pug');
 app.set('views','./views');
 
@@ -14,12 +19,20 @@ app.get('/', function(req,res) {
 
 app.get('/user', function(req,res) {
     res.render('users/index',{
-      users: [
-        {id:1, name:'Khoa'},
-        {id:2, name:'Khoa2'}
-      ]
+      users : users
     });  
 });
+
+app.get('/users/search',function(req,res) {
+  var q = req.query.q;
+  var matchdUsers = users.filter(function(user){
+    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+  res.render('users/index',{
+    users : matchdUsers
+  });
+})
+
 
 app.listen(port, function() {
     console.log('server listening on port ' + port)
