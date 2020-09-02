95ed721c07a0e2381bfde9f7e4fba4cf92fbfa81
Tran Khoa <khoalt.pht@gmail.com>
2020-09-02 17:22:39 +0700

    learn template engines

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine','pug');
+app.set('views','./views');
+
 app.get('/', function(req,res) {
-  res.send('<h1>Hello world</h1>');  
+  res.render('index',{
+    name:'AAA'
+  });  
 });
 
 app.get('/user', function(req,res) {
-    res.send('User list');  
+    res.render('users/index',{
+      users: [
+        {id:1, name:'Khoa'},
+        {id:2, name:'Khoa2'}
+      ]
+    });  
 });
 
 app.listen(port, function() {
